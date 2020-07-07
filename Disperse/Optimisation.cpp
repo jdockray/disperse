b87b31dfa1f152022d02e49ebf0d70eb31a92f9a
jdockray <jdockray@hotmail.com>
2020-07-07 19:29:55 +0100

    Optimisation connector code refactoring

---
+++ Disperse/Optimisation.cpp
@@ -0,0 +1,157 @@
+
+
+#include "Optimisation.hpp"
+
+SparseMatrixElement::SparseMatrixElement(c_int rowIndex, c_int columnIndex, c_float value)
+	: rowIndex(rowIndex), columnIndex(columnIndex), value(value)
+{
+}
+
+bool operator<(const SparseMatrixElement& a, const SparseMatrixElement& b)
+{
+	if (a.columnIndex != b.columnIndex) return a.columnIndex < b.columnIndex;
+	return a.rowIndex < b.rowIndex;
+}
+
+SafeCSC safeCSCFromDlibMatrixUpperTriangular(dlib::matrix<double> matrix)
+{
+	std::vector<SparseMatrixElement> vectorOfSparseElements;
+	for (int i = 0; i < matrix.nr(); ++i)
+	{
+		for (int j = i; j < matrix.nc(); ++j)
+		{
+			vectorOfSparseElements.push_back(SparseMatrixElement(i, j, matrix(i, j)));
+		}
+	}
+	return SafeCSC(vectorOfSparseElements, matrix.nr(), matrix.nc());
+}
+
+SafeCSC::SafeCSC(const std::vector<SparseMatrixElement>& vectorOfSparseElements,
+	c_int matrixRowNumber, c_int matrixColumnNumber)
+{
+	std::sort(vectorOfSparseElements.front(), vectorOfSparseElements.back());
+	c_int currentColumn = -1;
+	for (SparseMatrixElement element : vectorOfSparseElements)
+	{
+		if (element.columnIndex > currentColumn) columnPointers.push_back(rowIndices.size());
+		rowIndices.push_back(element.rowIndex);
+		values.push_back(element.value);
+	}
+	nzmax = rowIndices.size();
+	m = matrixRowNumber;
+	n = matrixColumnNumber;
+	p = columnPointers.data();
+	i = rowIndices.data();
+	x = values.data();
+	nz = -1;
+}
+
+DisperseOSQPSettings::DisperseOSQPSettings()
+{
+	osqp_set_default_settings(this);
+}
+
+void WorkspaceDeleter::operator()(OSQPWorkspace* pOsqpWorkspace)
+{
+	if (pOsqpWorkspace)
+	{
+		osqp_cleanup(pOsqpWorkspace);
+	}
+}
+
+std::unique_ptr<OSQPWorkspace, WorkspaceDeleter> callOSQPSetup(OSQPData& osqpData)
+{
+	OSQPWorkspace* pOsqpWorkspace = NULL;
+	switch (static_cast<SetupExitStatus>(osqp_setup(&pOsqpWorkspace, &osqpData, &DISPERSE_OSQP_SETTINGS)))
+	{
+	case SetupExitStatus::SUCCESS:
+		break;
+	case SetupExitStatus::MEM_ALLOC_ERROR:
+		throw InsufficientMemoryException();
+	case SetupExitStatus::LINSYS_SOLVER_LOAD_ERROR:
+	case SetupExitStatus::LINSYS_SOLVER_INIT_ERROR:
+		throw SolverInitialisationException();
+	case SetupExitStatus::DATA_VALIDATION_ERROR:
+	case SetupExitStatus::SETTINGS_VALIDATION_ERROR:
+	case SetupExitStatus::WORKSPACE_NOT_INIT_ERROR:
+	case SetupExitStatus::NONCVX_ERROR:
+	default:
+		throw UnexpectedException();
+	}
+	if (!pOsqpWorkspace)
+	{
+		throw UnexpectedException();
+	}
+	return std::unique_ptr<OSQPWorkspace, WorkspaceDeleter>(pOsqpWorkspace, WorkspaceDeleter());
+}
+
+std::vector<double> callOSQPSolve(OSQPWorkspace& osqpWorkspace)
+{
+	switch (static_cast<SolveExitStatus>(osqp_solve(&osqpWorkspace)))
+	{
+	case SolveExitStatus::SOLVED_INACCURATE:
+	case SolveExitStatus::MAX_ITER_REACHED:
+		// Solution accepted in release mode but we must also test that results are acceptable in debug mode
+#ifndef _DEBUG
+		throw UnexpectedException();
+#endif
+	case SolveExitStatus::SOLVED:
+		return;
+	case SolveExitStatus::SIGINT:
+		throw OptimisationInterruptedException();
+	case SolveExitStatus::PRIMAL_INFEASIBLE:
+	case SolveExitStatus::PRIMAL_INFEASIBLE_INACCURATE:
+	case SolveExitStatus::DUAL_INFEASIBLE:
+	case SolveExitStatus::DUAL_INFEASIBLE_INACCURATE:
+	case SolveExitStatus::NON_CVX:
+	case SolveExitStatus::UNSOLVED:
+	default:
+		throw UnexpectedException();
+	}
+	if (!osqpWorkspace.solution || !osqpWorkspace.solution->x)
+	{
+		throw UnexpectedException();
+	}
+	return std::vector<double>(osqpWorkspace.solution->x, osqpWorkspace.solution->x + osqpWorkspace.data->n);
+}
+
+std::vector<double> solve(double minimumReturn, const std::vector<Security>& securities, dlib::matrix<double> covarianceMatrix)
+{
+	std::vector<c_float> qVector(securities.size());
+	std::vector<unsigned int> indicesOfSecuritiesWithLimits;
+	std::vector<c_float> lVector;
+	lVector.push_back(0);
+	std::vector<c_float> uVector;
+	uVector.push_back(1);
+	std::vector<SparseMatrixElement> elementsOfA;
+	for (int i = 0; i < securities.size(); ++i)
+	{
+		const Security& security = securities.at(i);
+		elementsOfA.push_back(SparseMatrixElement(i, 0, 1)); // For sum of all allocations
+		if (security.getMinProportion() > 0 && security.getMaxProportion())
+		{
+			indicesOfSecuritiesWithLimits.push_back(i);
+			lVector.push_back(security.getMinProportion());
+			uVector.push_back(security.getMaxProportion());
+			elementsOfA.push_back(SparseMatrixElement(i, i + 1, 1));
+		}
+	}
+
+	OSQPData osqpData;
+	osqpData.n = securities.size();
+	osqpData.m = securities.size() + 1;
+
+	SafeCSC cscP = safeCSCFromDlibMatrixUpperTriangular(covarianceMatrix);
+	osqpData.P = &cscP;
+
+	SafeCSC cscA(elementsOfA, osqpData.m, osqpData.n);
+	osqpData.A = &cscA;
+
+	osqpData.q = qVector.data();
+	osqpData.l = lVector.data();
+	osqpData.u = uVector.data();
+
+	std::unique_ptr<OSQPWorkspace, WorkspaceDeleter> osqpWorkspace = callOSQPSetup(osqpData);
+
+	return callOSQPSolve(*osqpWorkspace);
+}
--- Disperse/Optimisation.hpp
+++ Disperse/Optimisation.hpp
@@ -3,6 +3,7 @@
 #ifndef DISPERSE_OPTIMISATION
 #define DISPERSE_OPTIMISATION
 
+#include "Security.hpp"
 #include "dlib\matrix.h"
 #include "osqp\include\osqp.h"
 #include <vector>
@@ -35,60 +36,27 @@ enum class SolveExitStatus
 
 struct SparseMatrixElement
 {
-	SparseMatrixElement(c_int rowIndex, c_int columnIndex, c_float value)
-		: rowIndex(rowIndex), columnIndex(columnIndex), value(value)
-	{
-	}
+	SparseMatrixElement(c_int rowIndex, c_int columnIndex, c_float value);
 
 	c_int rowIndex;
 	c_int columnIndex;
 	c_float value;
 };
 
-bool operator<(const SparseMatrixElement& a, const SparseMatrixElement& b)
-{
-	if (a.columnIndex != b.columnIndex) return a.columnIndex < b.columnIndex;
-	return a.rowIndex < b.rowIndex;
-}
+bool operator<(const SparseMatrixElement& a, const SparseMatrixElement& b);
 
-SafeCSC safeCSCFromDlibMatrixUpperTriangular(dlib::matrix<double> matrix)
-{
-	std::vector<SparseMatrixElement> vectorOfSparseElements;
-	for (int i = 0; i < matrix.nr; ++i)
-	{
-		for (int j = i; j < matrix.nc; ++j)
-		{
-			vectorOfSparseElements.push_back(SparseMatrixElement(i, j, matrix(i, j)));
-		}
-	}
-	return SafeCSC(vectorOfSparseElements, matrix.nr, matrix.nc);
-}
+SafeCSC safeCSCFromDlibMatrixUpperTriangular(dlib::matrix<double> matrix);
 
-// dlib::upperm can be used to make an upper triangular matrix
 class SafeCSC : public csc
 {
 public:
-	SafeCSC(const std::vector<SparseMatrixElement> &vectorOfSparseElements,
-		c_int matrixRowNumber, c_int matrixColumnNumber)
-	{
-		std::sort(vectorOfSparseElements.front(), vectorOfSparseElements.back());
-		c_int currentColumn = -1;
-		for (SparseMatrixElement element : vectorOfSparseElements)
-		{
-			if (element.columnIndex > currentColumn) columnPointers.push_back(rowIndices.size());
-			rowIndices.push_back(element.rowIndex);
-			values.push_back(element.value);
-		}
-		nzmax = rowIndices.size();
-		m = matrixRowNumber;
-		n = matrixColumnNumber;
-		p = columnPointers.data();
-		i = rowIndices.data();
-		x = values.data();
-		nz = -1;
-	}
+	SafeCSC(const std::vector<SparseMatrixElement>& vectorOfSparseElements,
+		c_int matrixRowNumber, c_int matrixColumnNumber);
 
 private:
+	SafeCSC(SafeCSC const&) = delete;
+	SafeCSC& operator=(SafeCSC const&) = delete;
+
 	std::vector<c_int> columnPointers;
 	std::vector<c_int> rowIndices;
 	std::vector<c_float> values;
@@ -96,134 +64,18 @@ private:
 
 static const class DisperseOSQPSettings : public OSQPSettings
 {
-	DisperseOSQPSettings()
-	{
-		osqp_set_default_settings(this);
-	}
-
+	DisperseOSQPSettings();
 } DISPERSE_OSQP_SETTINGS;
 
-
-class ScopedOSQPWorkspaceUsage
+struct WorkspaceDeleter
 {
-public:
-	ScopedOSQPWorkspaceUsage(OSQPWorkspace& workspace)
-		: m_workspace(workspace)
-	{
-	}
-	
-	OSQPWorkspace &getWorkspace()
-	{
-		return m_workspace;
-	}
-
-	~ScopedOSQPWorkspaceUsage()
-	{
-		osqp_cleanup(&m_workspace);
-	}
-
-private:
-	OSQPWorkspace& m_workspace;
+	void operator()(OSQPWorkspace* pOsqpWorkspace);
 };
-	
-OSQPWorkspace& callOSQPSetup(OSQPData &osqpData)
-{
-	OSQPWorkspace* pOsqpWorkspace = NULL;
-	switch (static_cast<SetupExitStatus>(osqp_setup(&pOsqpWorkspace, &osqpData, &DISPERSE_OSQP_SETTINGS)))
-	{
-	case SetupExitStatus::SUCCESS:
-		break;
-	case SetupExitStatus::MEM_ALLOC_ERROR:
-		throw InsufficientMemoryException();
-	case SetupExitStatus::LINSYS_SOLVER_LOAD_ERROR:
-	case SetupExitStatus::LINSYS_SOLVER_INIT_ERROR:
-		throw SolverInitialisationException();
-	case SetupExitStatus::DATA_VALIDATION_ERROR:
-	case SetupExitStatus::SETTINGS_VALIDATION_ERROR:
-	case SetupExitStatus::WORKSPACE_NOT_INIT_ERROR:
-	case SetupExitStatus::NONCVX_ERROR:
-	default:
-		throw UnexpectedException();
-	}
-	if (!pOsqpWorkspace)
-	{
-		throw UnexpectedException();
-	}
-	return *pOsqpWorkspace;
-}
 
-void callOSQPSolve(ScopedOSQPWorkspaceUsage& workspaceUsage)
-{
-	switch (static_cast<SolveExitStatus>(osqp_solve(&workspaceUsage.getWorkspace())))
-	{
-	case SolveExitStatus::SOLVED_INACCURATE:
-	case SolveExitStatus::MAX_ITER_REACHED:
-// Solution accepted in release mode but we must also test that results are acceptable in debug mode
-#ifndef _DEBUG
-		throw UnexpectedException();
-#endif
-	case SolveExitStatus::SOLVED:
-		return;
-	case SolveExitStatus::SIGINT:
-		throw OptimisationInterruptedException();
-	case SolveExitStatus::PRIMAL_INFEASIBLE:
-	case SolveExitStatus::PRIMAL_INFEASIBLE_INACCURATE:
-	case SolveExitStatus::DUAL_INFEASIBLE:
-	case SolveExitStatus::DUAL_INFEASIBLE_INACCURATE:
-	case SolveExitStatus::NON_CVX:
-	case SolveExitStatus::UNSOLVED:
-	default:
-		throw UnexpectedException();
-	}
-}
+std::unique_ptr<OSQPWorkspace, WorkspaceDeleter> callOSQPSetup(OSQPData& osqpData);
 
-void solve(double minimumReturn, const std::vector<Security>& securities, dlib::matrix<double> covarianceMatrix)
-{
-	std::vector<c_float> qVector(securities.size());
-	std::vector<unsigned int> indicesOfSecuritiesWithLimits;
-	std::vector<c_float> lVector;
-	lVector.push_back(0);
-	std::vector<c_float> uVector;
-	uVector.push_back(1);
-	std::vector<SparseMatrixElement> elementsOfA;
-	for (int i = 0; i < securities.size(); ++i)
-	{
-		const Security& security = securities.at(i);
-		elementsOfA.push_back(SparseMatrixElement(i, 0, 1)); // For sum of all allocations
-		if (security.getMinProportion() > 0 && security.getMaxProportion())
-		{
-			indicesOfSecuritiesWithLimits.push_back(i);
-			lVector.push_back(security.getMinProportion());
-			uVector.push_back(security.getMaxProportion());
-			elementsOfA.push_back(SparseMatrixElement(i, i + 1, 1));
-		}
-	}
+std::vector<double> callOSQPSolve(OSQPWorkspace& osqpWorkspace);
 
-	OSQPData osqpData;
-	osqpData.n = securities.size();
-	osqpData.m = securities.size() + 1;
-
-	SafeCSC cscP = safeCSCFromDlibMatrixUpperTriangular(covarianceMatrix);
-	osqpData.P = &cscP;
-
-	SafeCSC cscA(elementsOfA, osqpData.m, osqpData.n);
-	osqpData.A = &cscA;
-
-	osqpData.q = qVector.data();
-	osqpData.l = lVector.data();
-	osqpData.u = uVector.data();
-
-	OSQPWorkspace osqpWorkspace = callOSQPSetup(osqpData);
-	
-	// Check pOsqpWorkspace not null, asserting and throwing if necessary, maybe in scoped usage constructor
-
-	ScopedOSQPWorkspaceUsage scopedWorkspaceUsage(osqpWorkspace);
-
-	callOSQPSolve(scopedWorkspaceUsage);
-
-
-
-
-}
+std::vector<double> solve(double minimumReturn, const std::vector<Security>& securities, dlib::matrix<double> covarianceMatrix);
 
 #endif // #ifndef DISPERSE_OPTIMISATION
