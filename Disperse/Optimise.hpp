16dcf1c13a4149242b29ee1bb5798fb686f4edf0
jdockray <jdockray@hotmail.com>
2023-03-05 00:17:09 +0000

    Missing include guard

--- Disperse/Optimise.hpp
+++ Disperse/Optimise.hpp
@@ -1,7 +1,12 @@
 
+#ifndef DISPERSE_OPTIMISE
+#define DISPERSE_OPTIMISE
+
 #pragma warning(push, 0)
 #include <vector>
 #include <string>
 #pragma warning(pop)
 
 void runOptimiseCommand(const std::vector<std::string>& args);
+
+#endif // DISPERSE_ELEMENTS
