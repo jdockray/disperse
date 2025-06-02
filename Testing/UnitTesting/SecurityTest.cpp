4801f40d15b11a8b78c7242b2b06ce5da19d25c7
jdockray <12460717+jdockray@users.noreply.github.com>
2025-06-02 01:20:11 +0100

    ListOfSecurities tests

--- Testing/UnitTesting/SecurityTest.cpp
+++ Testing/UnitTesting/SecurityTest.cpp
@@ -3,15 +3,6 @@
 
 #include "gtest/gtest.h"
 
-/*
-class ListOfSecurities : public IdentifiedObjectList<Security>, public ProportionLimitedObjectList<Security> {
-public:
-	const std::vector<Security>& getObjects() const;
-	std::vector<std::string> getAllFactors() const;
-	std::set<std::string> getAllGroups() const;
-};
-*/
-
 TEST(Security, SetThenGet) {
 	static const double expectedReturn = -0.5;
 	static const double risk = 1.5;
@@ -46,4 +37,48 @@ TEST(Security, ExposureAddAndResidualRecalculate) {
 
 TEST(Security, ExposuresExceedOneFail) {
 	EXPECT_THROW(Security("").addExposure("", 1.1), InvalidLimitSumException);
+}
+
+TEST(ListOfSecurities, GetObjectsHasPublicScope) {
+	ListOfSecurities().getObjects();
+}
+
+template<typename KEY, typename VALUE>
+void addMapKeysToSet(const std::map<KEY, VALUE>& map, std::set<KEY>& set) {
+	for (const std::pair<KEY, VALUE>& keyValuePair : map) {
+		set.insert(keyValuePair.first);
+	}
+}
+
+TEST(ListOfSecurities, GetAllFactors) {
+	static const std::string factor_x = "Factor X";
+	static const std::string factor_y = "Factor Y";
+	std::set<std::string> expectedFactorSet;
+	Security securityA("Security A");
+	securityA.addExposure(factor_x, 0.5);
+	addMapKeysToSet(securityA.getExposures(), expectedFactorSet);
+	Security securityB("Security B");
+	securityB.addExposure(factor_x, 0.3);
+	securityB.addExposure(factor_y, 0.3);
+	addMapKeysToSet(securityB.getExposures(), expectedFactorSet);
+	ListOfSecurities listOfSecurities;
+	listOfSecurities.add(securityA);
+	listOfSecurities.add(securityB);
+	for (const std::string& factor : listOfSecurities.getAllFactors()) {
+		EXPECT_EQ(expectedFactorSet.erase(factor), 1); // One element should be removed
+	}
+	EXPECT_EQ(expectedFactorSet.size(), 0); // No more factors should be expected
+}
+
+TEST(ListOfSecurities, GetAllGroups) {
+	static const std::string group_name = "Test group";
+	ListOfSecurities listOfSecurities;
+	EXPECT_EQ(listOfSecurities.getAllGroups().size(), 0);
+	Security security("Test security");
+	security.setGroup(group_name);
+	listOfSecurities.add(security);
+	security.setGroup("");
+	std::set<std::string> groups = listOfSecurities.getAllGroups();
+	ASSERT_EQ(groups.size(), 1);
+	EXPECT_EQ(*groups.cbegin(), group_name);
 }
